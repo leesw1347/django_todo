40e247e007091f819145f620f87bc8b62a4a5fbc
leesw1347 <leesw1347@gmail.com>
2022-07-13 05:25:08 +0900

    [DEVELOPMENT] admin 페이지에서 todo app에 대한 관리할 수 있도록 한다

--- django/Projects/DRF_Projects/03_DjangoTodo/mytodo/mytodo/urls.py
+++ django/Projects/DRF_Projects/03_DjangoTodo/mytodo/mytodo/urls.py
@@ -11,14 +11,14 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatteWWrns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
 
-# import todo
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('todo/', todo.urls)
+
+    # TODO 앱을 관리자페이지에서 접속하려면 url을 설정해줘야한다
+
 ]
--- django/Projects/DRF_Projects/03_DjangoTodo/mytodo/todo/admin.py
+++ django/Projects/DRF_Projects/03_DjangoTodo/mytodo/todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+# 관리자 페이지에서 todo 앱을 확인할 수 있도록 등록한다
+from todo.models import Todo
+
 # Register your models here.
+
+admin.site.register(Todo)
+
